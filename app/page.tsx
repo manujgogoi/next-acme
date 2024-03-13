827a03e1df438df7709b1d849873443e014a47e8
Manuj Gogoi <manujgogoi@gmail.com>
2024-03-13 09:23:20 +0530

    Active link styles updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import styles from '@/app/ui/home.module.css'
+import styles from '@/app/ui/home.module.css';
 import { inconsolata, lusitana, pacifico, roboto } from './ui/fonts';
 import Image from 'next/image';
 
@@ -13,19 +13,17 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div
-  className={styles.shape}/>
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}>
+          <div className={styles.shape} />
+          <p
+            className={`text-xl text-gray-800 md:text-3xl md:leading-normal ${lusitana.className} antialiased`}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
-          {/* <p className={`${pacifico.className} antialiased text-4xl`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, rem eveniet. Reiciendis provident ab aspernatur ad molestias tempore nesciunt soluta.</p> */}
-          {/* <p className={`${roboto.className} antialiased text-4xl`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, rem eveniet. Reiciendis provident ab aspernatur ad molestias tempore nesciunt soluta.</p> */}
-          {/* <p className={`${inconsolata.className} antialiased text-4xl`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, rem eveniet. Reiciendis provident ab aspernatur ad molestias tempore nesciunt soluta.</p> */}
-          
+
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -35,18 +33,18 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
-          <Image 
+          <Image
             src="/hero-desktop.png"
             width={1000}
             height={760}
-            className='hidden md:block'
+            className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
-                    <Image 
+          <Image
             src="/hero-mobile.png"
             width={560}
             height={620}
-            className='block md:hidden'
+            className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
